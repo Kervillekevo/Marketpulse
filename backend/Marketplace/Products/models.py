a8aa02c376e8cb9223f81d278864f893c06769c3
Kervillekevo <kelvinngui00@gmail.com>
2026-04-20 23:00:14 +0300

    Use CloudinaryField for product images

--- backend/Marketplace/Products/models.py
+++ backend/Marketplace/Products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class Category(models.Model):
@@ -76,7 +77,7 @@ class ProductImage(models.Model):
         related_name="images",
         on_delete=models.CASCADE
     )
-    image = models.ImageField(upload_to="product_images/")
+    image = CloudinaryField('image')
 
     def __str__(self):
         return f"Image for {self.product.title}"
